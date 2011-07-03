1c757098a4cbc1fe3ce1bad0456a7233b8f6c0fc
Michael Sloan <mgsloan@lappy.(none)>
2011-07-02 17:28:57 -0700

    Change name to rex + documented syntax / examples

--- Test.hs
+++ Test.hs
@@ -1,23 +1,43 @@
 {-# LANGUAGE TemplateHaskell, QuasiQuotes, ViewPatterns #-}
 
-import Text.Regex.PCRE.QQT
+import Text.Regex.PCRE.Rex
+import Data.Maybe (catMaybes)
 
 math x = mathl x 0
 
-mathl [reg|^({ Just y }\d+)\s*({ id -> s }.*)|] x = mathl s y
-mathl [reg|^\+\s*({ Just y }\d+)\s*({ id -> s }.*)$|] x = mathl s $ x + y
-mathl [reg|^-\s*({ Just y }\d+)\s*({ id -> s }.*)$|]  x = mathl s $ x - y
-mathl [reg|^\*\s*({ Just y }\d+)\s*({ id -> s}.*)$|]  x = mathl s $ x * y
-mathl [reg|^/\s*({ Just y }\d+)\s*({ id -> s}.*)$|]   x = mathl s $ x / y
+mathl [rex|^  \s*(?{ Just y }\d+)\s*(?{ id -> s }.*)|]  x = mathl s y
+mathl [rex|^\+\s*(?{ Just y }\d+)\s*(?{ id -> s }.*)$|] x = mathl s $ x + y
+mathl [rex|^ -\s*(?{ Just y }\d+)\s*(?{ id -> s }.*)$|] x = mathl s $ x - y
+mathl [rex|^\*\s*(?{ Just y }\d+)\s*(?{ id -> s }.*)$|] x = mathl s $ x * y
+mathl [rex|^ /\s*(?{ Just y }\d+)\s*(?{ id -> s }.*)$|] x = mathl s $ x / y
 mathl [] x = x
 
-{-
-*Main> math "2 + 2"
-4.0
-*Main> math "2 + 2 * 5"
-20.0
-*Main> math "2 + 20 * 5"
-110.0
-*Main> math "2 + 20 * 5 / 3"
-36.666666666666664
--}
+peano :: String -> Int
+peano = [rex|^(?{ length . filter (=='S') } \s* (?:S\s+)*Z)\s*$|]
+
+vect2d :: String -> (Maybe Int, Maybe Int)
+vect2d = [rex|^<\s* (?{}\d+) \s*,\s* (?{}\d+) \s*>$|]
+
+-- From http://www.regular-expressions.info/dates.html
+parseDate :: String -> Maybe (Int, Int, Int)
+parseDate [rex|^(?{ Just y }(?:19|20)\d\d)[- /.]
+                (?{ Just m }0[1-9]|1[012])[- /.]
+                (?{ Just d }0[1-9]|[12][0-9]|3[01])$|]
+  |  (d > 30 && (m `elem` [4, 6, 9, 11]))
+  || (m == 2 &&
+       (d ==29 && not (mod y 4 == 0 && (mod y 100 /= 0 || mod y 400 == 0)))
+    || (d > 29)) = Nothing
+
+  | otherwise = Just (y, m, d)
+
+onNull a f [] = a
+onNull _ f xs = f xs
+
+nonNull  = onNull Nothing
+
+disjunct [rex| ^(?:(?{nonNull $ Just . head -> a} .)
+             | (?{nonNull $ Just . head -> b} ..)
+             | (?{nonNull $ Just . last -> c} ...))$|] =
+  head $ catMaybes [a, b, c]
+
+
--- Text/Regex/PCRE/Rex.hs
+++ Text/Regex/PCRE/Rex.hs
@@ -2,38 +2,106 @@
 
 -----------------------------------------------------------------------------
 -- |
--- Module      :  Text.Regex.PCRE.QQT
--- Copyright   :  Michael Sloan 2011
+-- Module      :  Text.Regex.PCRE.Rex
+-- Copyright   :  (c) Michael Sloan 2011
 --
 -- Maintainer  :  Michael Sloan (mgsloan@gmail.com)
+-- Stability   :  unstable
 -- Portability :  unportable
--- Stability   :  experimental
 -- 
 -- This module provides a template Haskell quasiquoter for regular
 -- expressions, which provides the following features:
 -- 
---   1) Compile-time checking that the regular expression is valid.
+-- 1) Compile-time checking that the regular expression is valid.
 -- 
---   2) Arity of resulting tuple based on the number of capture patterns in
---   the regular expression.
+-- 2) Arity of resulting tuple based on the number of selected capture patterns
+-- in the regular expression.
 --
---   3) By default utilizes type inference to determine how to parse capture
---   patterns - uses "maybeRead" function, which yields a "(Read a) => Just a"
---   value on successful parse.
+-- 3) By default utilizes type inference to determine how to parse capture
+-- patterns - uses "maybeRead" function, which yields a "(Read a) => Just a"
+-- value on successful parse.
 --
---   4) Allows for the inline interpolation of a mapping function String -> a.
+-- 4) Allows for the inline interpolation of a mapping function String -> a.
+--
+-- Here's a silly example which parses peano numbers of the form "Z", "S Z",
+-- "S S Z", etc.  The \s+ means that it is not sensitive to the quantity or type
+-- of seperating whitespace. (these examples can also be found in Test.hs)
+--
+-- > peano :: String -> Int
+-- > peano = [rex|^(?{ length . filter (=='S') } \s* (?:S\s+)*Z)\s*$|]
+--
+-- > *Main> peano "Z"
+-- > 0
+-- > *Main> peano "S Z"
+-- > 1
+-- > *Main> peano "S   S Z"
+-- > 2
+-- > *Main> peano "S S S Z"
+-- > 3
+-- > *Main> peano "invalid"
+-- > 0
+--
+-- The token \"(?{\" introduces a capture group which has a mapping applied to the
+-- result - in this case \"length . filter (=='S')\".  In the case that the match
+-- fails, \"\" is provided as input.  \"}\" ends the Haskell mapping expression.
+--
+-- If an expression is omitted, \"(?{} ... )\", then there is an implicit usage of
+-- the \"maybeRead :: (Read a) => String -> Maybe a\" function provided by this
+-- module.  If the ?{ ... } is omitted, then the capture group is not taken as
+-- part of the results of the match.
+-- 
+-- > vect2d :: String -> (Maybe Int, Maybe Int)
+-- > vect2d = [rex|^<\s* (?{}\d+) \s*,\s* (?{}\d+) \s*>$|]
+--
+-- The following example is derived from http://www.regular-expressions.info/dates.html
+--
+-- > parseDate :: String -> Maybe (Int, Int, Int)
+-- > parseDate [rex|^(?{ Just y }(?:19|20)\d\d)[- /.]
+-- >                 (?{ Just m }0[1-9]|1[012])[- /.]
+-- >                 (?{ Just d }0[1-9]|[12][0-9]|3[01])$|]
+-- >   |  (d > 30 && (m `elem` [4, 6, 9, 11]))
+-- >   || (m == 2 &&
+-- >        (d ==29 && not (mod y 4 == 0 && (mod y 100 /= 0 || mod y 400 == 0)))
+-- >     || (d > 29)) = Nothing
+-- > 
+-- >   | otherwise = Just (y, m, d)
+--
+-- The above example makes use of the regex quasi-quoter as a pattern matcher.
+-- The interpolated Haskell patterns are used to construct an implicit view
+-- pattern.  The above pattern is expanded to the equivalent:
+--
+-- > parseDate ([rex|^(?{}(?:19|20)\d\d)[- /.]
+-- >                  (?{}0[1-9]|1[012])[- /.]
+-- >                  (?{}0[1-9]|[12][0-9]|3[01])$|]
+-- >           -> (Just y, Just m, Just d))
+--
+-- Since this is how the patterns desugar, if you wish for the pattern to fall
+-- through to the next case on failed regex match, then at least one of the 
+-- captures needs to fail on the empty string.
+--
+-- In order to provide a capture-mapper along with a pattern, use view-patterns
+-- inside the interpolation brackets.
+--
+-- Caveat: Since haskell-src-exts does not support parsing view-patterns, the
+-- above is implemented as a relatively naive split on \"->\".  It presumes that
+-- the last \"->\" in the interpolated pattern seperates the pattern from an
+-- expression on the left.  This allows for lambdas to be present in the
+-- expression, but prevents nesting view patterns.
 --
 -- Inspired by / copy-modified from Matt Morrow's regexqq package:
 -- http://hackage.haskell.org/packages/archive/regexqq/latest/doc/html/src/Text-Regex-PCRE-QQ.html
 -- And code from Erik Charlebois's interpolatedstring-qq package:
 -- http://hackage.haskell.org/packages/archive/interpolatedstring-qq/latest/doc/html/Text-InterpolatedString-QQ.html
+--
+-----------------------------------------------------------------------------
 
-module Text.Regex.PCRE.QQT (reg, makeExpr, maybeRead, pack, unpack) where
+module Text.Regex.PCRE.Rex (rex, makeExpr, maybeRead, pack, unpack) where
 
 import Text.Regex.PCRE.Light (Regex,PCREOption,PCREExecOption)
 import qualified Text.Regex.PCRE.Light as PCRE
 
-import Control.Monad (liftM, join)
+import Control.Arrow (second)
+import Control.Monad (liftM)
 
 import qualified Data.ByteString as B
 import Data.ByteString.Internal (c2w,w2c)
@@ -62,8 +130,8 @@ type ParseChunks = (Int, String, [(Int, String)])
 {- TODO: target Text.Regex.Base -}
 
 -- | The regular expression quasiquoter.
-reg :: QuasiQuoter
-reg = QuasiQuoter
+rex :: QuasiQuoter
+rex = QuasiQuoter
         (checkRegex makeExpr . parseIt)
         (checkRegex makePat . parseIt)
         undefined undefined
@@ -71,7 +139,7 @@ reg = QuasiQuoter
 -- Gives an error at compile time if the regex string is invalid.
 checkRegex :: (ParseChunks -> a) -> ParseChunks -> a
 checkRegex f x@(_, pat, _) = 
-  forceEitherMsg ("Error compiling regular expression [$reg|"  ++ pat ++ "|]")
+  forceEitherMsg ("Error compiling regular expression [reg|"  ++ pat ++ "|]\n")
     (PCRE.compileM (pack pat) pcreOpts) `seq` f x
 
 
@@ -84,24 +152,24 @@ checkRegex f x@(_, pat, _) =
 -- the parser that doesn't exist
 makeExpr :: ParseChunks -> ExpQ
 makeExpr (cnt, pat, exs) = buildExpr cnt pat
-    . map (processExpr . snd . head)
+    . map ((>>= processExpr) . snd . head)
     . groupSortBy (comparing fst)
-    $ exs ++ ((0, "") : [(i, "maybeRead") | i <- [1..cnt]])
+    $ map (second Just) exs ++ [(i, Nothing) | i <- [0..cnt]]
   
 -- Creates the template haskell Pat which corresponds to the parsed interpolated
 -- regex. As well as handling the aforementioned defaulting considerations, this
 -- turns per-capture view patterns into a single tuple-resulting view pattern.
 -- 
--- E.g. [reg| ... ({e1 -> v1} ...) ... ({e2 -> v2} ...) ... |] becomes
---      [reg| ... ({e1} ...) ... ({e2} ...) ... |] -> (v1, v2)
+-- E.g. [reg| ... (?{e1 -> v1} ...) ... (?{e2 -> v2} ...) ... |] becomes
+--      [reg| ... (?{e1} ...) ... (?{e2} ...) ... |] -> (v1, v2)
 makePat :: ParseChunks -> PatQ
 makePat (cnt, pat, exs) = do
   viewExp <- buildExpr cnt pat $ map (liftM fst) ys
   return . ViewP viewExp . TupP . map snd $ catMaybes ys
  where
-  ys = map (join . liftM floatBoth . processView . snd . head)
+  ys = map ((>>= floatBoth) . processView . snd . head)
      . groupSortBy (comparing fst)
-     $ (0, "") : exs ++ [(i, "") | i <- [1..cnt]]
+     $ exs ++ [(i, "") | i <- [0..cnt]]
 
   processView "" = Nothing
   processView xs = case splitFromBack 2 ((split . onSublist) "->" xs) of
@@ -120,7 +188,7 @@ buildExpr cnt pat hexps = do
                $ PCRE.match (regex pat) (pack $(return $ VarE vx)) pcreExecOpts |]
   let mkMatch rcnt xs = Match
         (if null xs then ConP (mkName "Nothing") []
-                    else ConP (mkName "Just") . single . ListP $ map VarP xs)
+                    else ConP (mkName "Just") . single . ListP . (WildP:) $ map VarP xs)
         (NormalB . TupE . map (uncurry AppE) . catMaybes
                  . zipWith (curry floatFst) hexps 
                  $ (map VarE xs) ++ replicate rcnt emptyE)
@@ -130,15 +198,15 @@ buildExpr cnt pat hexps = do
 
 -- Parse a Haskell expression into a template Haskell Exp, yielding Nothing for
 -- strings which just consist of whitespace.
-processExpr :: [Char] -> Maybe Exp
-processExpr xs = onSpace Nothing
-  (Just . forceEitherMsg ("Error while parsing capture mapper " ++ xs) .  parseExp) xs
+processExpr :: String -> Maybe Exp
+processExpr xs = Just . forceEitherMsg ("Error while parsing capture mapper " ++ xs)
+               . parseExp . onSpace "maybeRead" id $ xs
 
 -- Parse a Haskell pattern match into a template Haskell Pat, yielding Nothing for
 -- strings which just consist of whitespace.
-processPat :: [Char] -> Maybe Pat
+processPat :: String -> Maybe Pat
 processPat xs = onSpace Nothing
-  (Just . forceEitherMsg ("Error while parsing capture mapper " ++ xs) .  parsePat) xs
+  (Just . forceEitherMsg ("Error while parsing capture pattern " ++ xs) . parsePat) xs
 
 -- Parsing
 -------------------------------------------------------------------------------
@@ -146,39 +214,36 @@ processPat xs = onSpace Nothing
 -- Postprocesses the results of the chunk-wise parse output, into the pattern to
 -- be pased to the regex engine, and the interpolated 
 parseIt :: String -> ParseChunks
-parseIt xs = (cnt, concat [x | Left x <- results]
+parseIt xs = ( cnt, concat [x | Left x <- results]
              , [(i, x) | Right (i, x) <- results])
-  where (cnt, results) = parseRegex (filter (`notElem` "\r\n") xs) "" True 0
+  where (cnt, results) = parseRegex (filter (`notElem` "\r\n") xs) "" (-1)
 
 -- A pair of mutually-recursive functions, one for processing the quotation
 -- and the other for the anti-quotation.
 
-parseRegex :: String -> String -> Bool -> Int -> (Int, [ParseChunk])
-parseRegex inp s b ix = case inp of
+-- TODO: add check for erroneous { }
+
+parseRegex :: String -> String -> Int -> (Int, [ParseChunk])
+parseRegex inp s ix = case inp of
   -- Disallow branch-reset capture.
   ('(':'?':'|':_) ->
     error "Branch reset pattern (?| not allowed in fancy quasi-quoted regex."
 
- 
-  -- Ignore non-capturing parens / handle backslash escaping
-  ('\\':'\\'  :xs) -> parseRegex xs ("\\\\" ++ s) False ix
-  ('\\':'('   :xs) -> parseRegex xs (")\\"  ++ s) False ix
-  ('\\':')'   :xs) -> parseRegex xs ("(\\"  ++ s) False ix
-  ('(':'?':':':xs) -> parseRegex xs (":?("  ++ s) False ix
+  -- Ignore non-capturing parens / handle backslash escaping.
+  ('\\':'\\'  :xs) -> parseRegex xs ("\\\\" ++ s) ix
+  ('\\':'('   :xs) -> parseRegex xs (")\\"  ++ s) ix
+  ('\\':')'   :xs) -> parseRegex xs ("(\\"  ++ s) ix
+  ('(':'?':':':xs) -> parseRegex xs (":?("  ++ s) ix
 
   -- Anti-quote for processing a capture group.
-  ('(':'{':xs) -> mapSnd ((Left $ reverse ('(':s)) :)
-                    $ parseHaskell xs "" (ix+1)
-                    
-  -- Anti-quote for processing the whole match. Only applies at the beginning.
-  ('{':xs) -> if b then parseHaskell xs "" ix
-                   else parseRegex xs ('{':s) False ix
+  ('(':'?':'{':xs) -> mapSnd ((Left $ reverse ('(':s)) :)
+                    $ parseHaskell xs "" (ix + 1)
 
   -- Keep track of how many capture groups we've seen.
-  ('(':xs) -> parseRegex xs ('(':s) False (ix+1)
+  ('(':xs) -> parseRegex xs ('(':s) (ix + 1)
 
   -- Consume the regular expression contents.
-  (x:xs) -> parseRegex xs (x:s) False ix
+  (x:xs) -> parseRegex xs (x:s) ix
   [] -> (ix, [Left $ reverse s])
 
 parseHaskell :: String -> String -> Int -> (Int, [ParseChunk])
@@ -188,7 +253,7 @@ parseHaskell inp s ix = case inp of
 
   -- Capture accumulated antiquote, and continue parsing regex literal.
   ('}':xs) -> mapSnd ((Right (ix, reverse s)):)
-            $ parseRegex xs "" False ix
+            $ parseRegex xs "" ix
 
   -- Consume the antiquoute contents, appending to a reverse accumulator.
   (x:xs) -> parseHaskell xs (x:s) ix
